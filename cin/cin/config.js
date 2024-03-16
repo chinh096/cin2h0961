5f5c282b4793e6e20ffd0bfa91bb9ef806bf529e
chinh096 <76523184+chinh096@users.noreply.github.com>
2024-03-16 14:39:14 +0700

    Add files via upload

---
+++ cin/cin/config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: "Hello",
+    introDesc: "Anh có chuyện muốn nói với em, nhớ phải trả lời thật lòng nha👉🏻👈🏻...",
+    btnIntro: "Oke",
+    title: "Làm người yêu anh nhé <3",
+    desc:  "Nếu em không trả lời mà thoát ra tức là đồng ý rồi đó nha 😍",
+    btnYes: "Đồng ý 😍...",
+    btnNo: "Không nha.",
+    question:"Nói lí do vì sao em yêu anh đi :>",
+    reasonPlaceholder: "Lý do",
+    btnReply: "Gửi tin nhắn",
+    reply: "Vì em lỡ thích anh từ lâu lắm rồi, mà không có dám nói 👉🏻👈🏻...",
+    mess: "Anh biết mà 😍 .Yêu em nhiều lắm 😘😘😘",
+    messDesc: "ib cho anh ngay nha...",
+    btnAccept: "Gửi tin nhắn",
+    messLink: "https://www.messenger.com/t/100030990041740/"
+}
